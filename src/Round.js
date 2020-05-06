8a93430050a2271449b4893a0eff124fefbc0430
Jeremiah Black <jeremiahblackol@gmail.com>
2020-05-06 16:29:47 -0600

    finished round class and testing

--- src/Round.js
+++ src/Round.js
@@ -1,5 +1,4 @@
 const Turn = require('../src/Turn');
-const Deck = require('../src/Deck');
 
 class Round {
   constructor(deck) {
@@ -8,6 +7,7 @@ class Round {
     this.incorrectGuesses = [];
     this.currentCard;
     this.turn;
+    this.deckLength = deck.countCards()
   }
 
   returnCurrentCard() {
@@ -33,79 +33,17 @@ class Round {
   }
 
   calculatePercentCorrect() {
-    //this.deck = new Deck (this.deck.cards)
-    console.log(this.deck)
-    // calculatePercentCorrect: method that 
-    //calculates and returns the percentage of 
-    //correct guesses
+    // eslint-disable-next-line max-len
+    return Math.floor(100 - ((this.incorrectGuesses.length / this.deckLength) * 100))
   }
 
-//   endRound() {
-//     // endRound: method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
+  endRound() {
+    // eslint-disable-next-line max-len
+    //console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
+    // eslint-disable-next-line max-len
+    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+  }
 }
 
 module.exports = Round;
 
-// // Your Round class will be the 
-// //object that takes in responses and 
-// //records these guesses 
-// //(as well as if they are correct or incorrect). 
-// //The currentCard should be the first Card 
-// //in the Deck (the array of Cards) at the 
-// //start of the Round
-
-// // Your Round class should meet the following requirements:
-
-// // When a guess is made, a new Turn instance 
-// //is created.
-
-
-// // The turns count is updated, 
-// //regardless of whether the guess is correct 
-// //or incorrect
-
-
-// // The next card becomes current card
-
-
-// // Guess is evaluated/recorded. 
-// //Incorrect guesses will be stored (via the id) 
-// //in an array of incorrectGuesses
-
-
-// // Feedback is returned regarding whether the guess 
-// //is incorrect or correct
-
-
-// // For example:
-
-// // const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-// // const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-// // const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-
-// // const deck = new Deck([card1, card2, card3]);
-
-// // const round = new Round(deck);
-
-
-
-
-
-// // round.turns; // => 0
-
-
-// // round.takeTurn('sea otter'); // => 'correct!'
-
-// // round.takeTurn('spleen');   // => 'incorrect!'
-
-// // round.turns; // => 2
-
-// // round.incorrectGuesses;     // => [14]
-
-// // round.returnCurrentCard();    // => { id: 12,
-// //             	              //      question: 'What is Travis\'s favorite stress reliever?',
-// //             	              //      answers: ['listening to music', 'watching Netflix', 'playing with bubble wrap'],
-// //             	              //      correctAnswer: 'playing with bubble wrap'
-// //             	              //    }
-
-// // round.calculatePercentCorrect(); // => 50
--- test/Round-test.js
+++ test/Round-test.js
@@ -12,8 +12,12 @@ describe('Round', function() {
     expect(Round).to.be.a('function');
   });
 
-  it('should be an instance of Round', function() {    
-    const round = new Round();
+  it('should be an instance of Round', function() { 
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
     expect(round).to.be.an.instanceof(Round);
   }); 
 
@@ -47,6 +51,37 @@ describe('Round', function() {
     expect(round.takeTurn).to.be.a('function')
     expect(round.takeTurn('sea otter')).to.equal('correct!')
     expect(round.takeTurn('horse shoe')).to.equal('incorrect!')
-    round.calculatePercentCorrect()
+  });
+
+  it('should be able to return the percentage of correct answers', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const card4 = new Card(26, 'Favorite color', ['red', 'blue', 'green'], 'blue')
+    const card5 = new Card(88, 'Favorite Rapper', ['3000', 'Jay-Z', 'Tupac'], '3000')
+    const deck = new Deck([card1, card2, card3, card4, card5]);
+    const round = new Round (deck)
+    round.takeTurn('dog')
+    round.takeTurn('tooth')
+    round.takeTurn('Fitzgerald')
+    round.takeTurn('blue')
+    round.takeTurn('3000')
+    expect(round.calculatePercentCorrect()).to.equal(60)
+  });
+
+  it('should be able to end the round', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const card4 = new Card(26, 'Favorite color', ['red', 'blue', 'green'], 'blue')
+    const card5 = new Card(88, 'Favorite Rapper', ['3000', 'Jay-Z', 'Tupac'], '3000')
+    const deck = new Deck([card1, card2, card3, card4, card5]);
+    const round = new Round (deck)
+    round.takeTurn('dog')
+    round.takeTurn('tooth')
+    round.takeTurn('Fitzgerald')
+    round.takeTurn('blue')
+    round.takeTurn('3000')
+    expect(round.endRound()).to.equal('** Round over! ** You answered 60% of the questions correctly!')
   });
 });
